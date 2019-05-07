354fcebfe335740534cc08ccd61d322098410946
MMangus <mjmangus16@yahoo.com>
2019-05-07 15:18:49 -0400

    completed callbacks and closures minus stretch goals

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,8 +1,8 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
-/* 
+/*
 
   //Given this problem: 
   
@@ -24,27 +24,41 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, length => console.log(length));
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+
+last(items, lastItem => console.log(lastItem));
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
+sumNums(5, 2, sum => console.log(sum));
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+
+multiplyNums(2, 4, total => console.log(total));
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  cb(list.includes(item) ? true : false);
 }
+
+contains("Gum", items, bool => console.log(bool));
 
 /* STRETCH PROBLEM */
 
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,9 +1,20 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+const outer = () => {
+  let b = 5;
+
+  function inner() {
+    let a = 10;
+
+    console.log(a + b);
+  }
+  return inner();
+};
+
+outer();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
-
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
